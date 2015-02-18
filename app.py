304c1bf30ab37b4d457b2a3281d808fe723b3569
Michael Leahy <mikel9286@hotmail.com>
2015-02-17 22:55:44 -0500

    basic bootle bootstrapping for heroku

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from os import environ as env
+from sys import argv
+
+import bottle
+from bottle import default_app, request, route, response, get
+
+bottle.debug(True)
+
+@get('/')
+def index():
+    response.content_type = 'text/plain; charset=utf-8'
+    return 'Hello bottle!'
+
+bottle.run(host='0.0.0.0', port=argv[1])

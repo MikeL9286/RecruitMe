ee85661ccff90dd95c93491e74f0a2fd6a6349c1
Michael Leahy <mikel9286@hotmail.com>
2015-02-22 01:34:50 -0500

    redirecting all role types to basic dashboard for now

--- controllers/authentication_controller.py
+++ controllers/authentication_controller.py
@@ -65,22 +65,28 @@ def logout():
 	return bottle.redirect('/')
 
 def render_dashboard_by_role(user):
-	if user.role == Role_Type.basic:
+	if user.role == Role_Type.recruit:
 		return bottle.template('dashboard_basic', {'user':user})
 
-	if user.role == Role_Type.recruit:
-		return bottle.template('dashboard_recruit', {'user':user})
-
 	if user.role == Role_Type.recruiter:
-		return bottle.template('dashboard_recruiter', {'user':user})
+		return bottle.template('dashboard_basic', {'user':user})
 
 	if user.role == Role_Type.admin:
-		return bottle.template('dashboard_admin', {'user':user})	
+		return bottle.template('dashboard_basic', {'user':user})	
+
+	return bottle.template('dashboard_basic', {'user':user})
 
 def seed_users():
 	user1 = user.User('user1@email.com', 'p1', 'User 1')
 	user2 = user.User('user2@email.com', 'p2', 'User 2')
 	user3 = user.User('user3@email.com', 'p3', 'User 3')
+	user4 = user.User('user4@email.com', 'p4', 'User 4')
+
+	user1.role = Role_Type.basic
+	user2.role = Role_Type.recruit
+	user3.role = Role_Type.recruiter
+	user4.role = Role_Type.admin
+
 	user_repo.seed_users([user1, user2, user3])
 
 auth_repo = authentication_repository.Authentication_Repository()
